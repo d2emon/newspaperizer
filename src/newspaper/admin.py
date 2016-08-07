471dae52f78490bc8a068309c038ea707703ebf0
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-07 16:33:44 +0300

    Issues

--- src/newspaper/admin.py
+++ src/newspaper/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from newspaper.models import Newspaper
+from newspaper.models import Newspaper, Year, Issue
 
 admin.site.register(Newspaper)
+admin.site.register(Year)
+admin.site.register(Issue)
--- src/newspaper/models.py
+++ src/newspaper/models.py
@@ -7,5 +7,30 @@ class Newspaper(models.Model):
     def __unicode__(self):
         return self.title
     
+    def __str__(self):
+        return self.__unicode__()
+
     def get_absolute_url(self):
         return "/newspaper/%i/" % self.id
+    
+
+class Year(models.Model):
+    year = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.year.__str__()
+    
+    def __str__(self):
+        return self.__unicode__()
+    
+class Issue(models.Model):
+    year = models.ForeignKey(Year)
+    newspaper = models.ForeignKey(Newspaper)
+    issue = models.IntegerField(default=0)
+    description = models.CharField(max_length=255, blank=True)
+    
+    def __unicode__(self):
+        return "{} {}'{} {}".format(self.newspaper, self.issue, self.year, self.description)
+        
+    def __str__(self):
+        return self.__unicode__()
