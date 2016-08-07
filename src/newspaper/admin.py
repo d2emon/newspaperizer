e01c555fd1eab4d9f67d02bfc9665514e9042d06
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-07 16:49:38 +0300

    New admin

--- src/newspaper/admin.py
+++ src/newspaper/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from newspaper.models import Newspaper, Year, Issue
 
-admin.site.register(Newspaper)
+
+class IssueInline(admin.TabularInline):
+    model = Issue
+    extra = 3
+    
+
+class NewspaperAdmin(admin.ModelAdmin):
+    inlines = [IssueInline]
+
+
 admin.site.register(Year)
-admin.site.register(Issue)
+admin.site.register(Newspaper, NewspaperAdmin)
