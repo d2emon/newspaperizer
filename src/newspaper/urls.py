6096570b9366414e0c98434aa7f88a8d6c3f141d
Dmitry Kutsenko <d2emonium@gmail.com>
2016-08-09 22:02:54 +0300

    Helloworld url

--- src/newspaper/urls.py
+++ src/newspaper/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 
 
 from newspaper.views import NewspaperListView, NewspaperDetailView
+import newspaper.views
 
 
 urlpatterns = [
     url(r'^$', NewspaperListView.as_view(), name='list'),
+    url(r'^hello$', newspaper.views.index, name='hello'),
     url(r'^(?P<pk>\d+)/$', NewspaperDetailView.as_view()),
 ]
--- src/newspaper/views.py
+++ src/newspaper/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from newspaper.models import Newspaper
 
 
+def index(request):
+    return HttpResponse("Hello world!")
+    
 class NewspaperListView(ListView):
     model = Newspaper
     
