05b655e934e2b07a9ed9835c039a786b05b89c2d
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-04 20:30:08 +0300

    Article list

--- src/newspaper/urls.py
+++ src/newspaper/urls.py
@@ -2,12 +2,12 @@
 from django.conf.urls import url
 
 
-from newspaper.views import NewspaperListView, YearListView, IssueListView, IssueDetailView
+from newspaper.views import NewspaperListView, YearListView, IssueListView, ArticleListView
 
 
 urlpatterns = [
     url(r'^$', NewspaperListView.as_view(), name='list'),
     url(r'^(?P<np>\d+)/$', YearListView.as_view(), name='year'),
     url(r'^(?P<np>\d+)/(?P<year>\d+)/$', IssueListView.as_view(), name='issues'),
-    url(r'^(?P<np>\d+)/(?P<year>\d+)/(?P<issue>\d+)/$', IssueDetailView.as_view(), name='issue'),
+    url(r'^(?P<np>\d+)/(?P<year>\d+)/(?P<issue>\d+)/$', ArticleListView.as_view(), name='issue'),
 ]
--- src/newspaper/views.py
+++ src/newspaper/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from newspaper.models import Newspaper, Year, Issue
+from article.models import Article
 
 
 class NewspaperListView(ListView):
@@ -12,7 +13,9 @@ class YearListView(ListView):
     
     def get_queryset(self):
         newspaper_id = self.kwargs.get('np')
+        
         np = Newspaper.objects.get(pk=newspaper_id)
+        
         return self.model.objects.filter(issue__newspaper=np).distinct()
     
     def get_context_data(self, **kwargs):
@@ -29,8 +32,10 @@ class IssueListView(ListView):
     def get_queryset(self):
         newspaper_id = self.kwargs.get('np')
         year_id = self.kwargs.get('year')
+        
         np = Newspaper.objects.get(pk=newspaper_id)
         year = Year.objects.get(year=year_id)
+        
         return self.model.objects.filter(newspaper=np, year=year).distinct()
     
     def get_context_data(self, **kwargs):
@@ -42,13 +47,32 @@ class IssueListView(ListView):
         return context
 
     
-class IssueDetailView(DetailView):
-    model = Issue
-    
-    def get_object(self):
+class ArticleListView(ListView):
+    model = Article
+
+    def get_queryset(self):
         newspaper_id = self.kwargs.get('np')
         year_id = self.kwargs.get('year')
         issue_id = self.kwargs.get('issue')
+        
         np = Newspaper.objects.get(pk=newspaper_id)
         year = Year.objects.get(year=year_id)
-        return self.model.objects.get(newspaper=np, year=year, issue=issue_id)
+        issue = Issue.objects.get(newspaper=np, year=year, issue=issue_id)
+        
+        return self.model.objects.filter(issue=issue).distinct()
+    
+    def get_context_data(self, **kwargs):
+        newspaper_id = self.kwargs.get('np')
+        year_id = self.kwargs.get('year')
+        issue_id = self.kwargs.get('issue')
+        
+        np = Newspaper.objects.get(pk=newspaper_id)
+        year = Year.objects.get(year=year_id)
+
+        context = ListView.get_context_data(self, **kwargs)
+        context.update({
+            "np": np,                        
+            "year": year,                        
+            "issue": Issue.objects.get(newspaper=np, year=year, issue=issue_id),                        
+        })
+        return context
