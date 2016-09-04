07d1aa19ab0553f0f56f2c5084538ae7ae84fe3e
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-04 14:01:19 +0300

    Comfort model editing

--- src/article/admin.py
+++ src/article/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from article.models import ArticleCategory, Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Newspaper', {'fields': (('issue', 'page'), 'category')}),
+        (None, {'fields': ('title', 'description')}),
+    )
+    
+
 admin.site.register(ArticleCategory)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
