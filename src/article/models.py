414e89cf85deeb4693ee1ce036794d498ad9c825
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-05 01:33:36 +0300

    Links article

--- src/article/models.py
+++ src/article/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+TEXT_ARTICLE = 1
+LINKS_ARTICLE = 2
+NOTES_ARTICLE = 3
+INTERVIEW_ARTICLE = 4
+
+
 class ArticleCategory(models.Model):
     title = models.CharField(max_length=255)
     newspaper = models.ForeignKey('newspaper.Newspaper', null=True)
@@ -21,7 +27,10 @@ class ArticleType(models.Model):
         return self.title
         
     def __str__(self):
-        return self.__unicode__()    
+        return self.__unicode__()   
+    
+    def is_links(self):
+        return self.pk == LINKS_ARTICLE 
    
     
 class Article(models.Model):
@@ -37,6 +46,12 @@ class Article(models.Model):
         
     def __str__(self):
         return self.__unicode__()
+    
+    def text(self):
+        if self.article_type.pk == NOTES_ARTICLE:
+            return "NOTES\n{}".format(self.description)
+        else:
+            return self.description
     
     class Meta:
         ordering = ['issue', 'page', 'title']        
