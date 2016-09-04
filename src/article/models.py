93f1f832d5b1011a7c8c690f8344e035fd7a7ed6
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-04 12:15:40 +0300

    Articles for page

--- src/article/models.py
+++ src/article/models.py
@@ -14,13 +14,14 @@ class ArticleCategory(models.Model):
    
     
 class Article(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     issue = models.ForeignKey('newspaper.Issue', null=True)
     category = models.ForeignKey('ArticleCategory', null=True)
-    description = models.TextField(max_length=10000)
+    page = models.IntegerField(default=1)
+    description = models.TextField(max_length=10000, null=True, blank=True)
     
     def __unicode__(self):
-        return "{} ({} - {})".format(self.title, self.issue, self.category.title)
+        return "{} ({}, {} - {})".format(self.title, self.issue, self.page, self.category.title)
         
     def __str__(self):
         return self.__unicode__()        
