4fc7f0bc0039d15947d68f194690ac1a484cbdaf
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-05 14:30:32 +0300

    Breadcrumbs

--- src/newspaperizer/settings.py
+++ src/newspaperizer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap_breadcrumbs',
     'article',
     'newspaper',
     'note',
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+BREADCRUMBS_TEMPLATE = "django_bootstrap_breadcrumbs/bootstrap3.html"
