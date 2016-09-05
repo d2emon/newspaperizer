af9d08c0e4b922e693acdbd4a715784a6201bbdf
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-05 15:52:53 +0300

    Navbar

--- src/newspaper/urls.py
+++ src/newspaper/urls.py
@@ -4,8 +4,8 @@ from newspaper.views import NewspaperListView, YearListView, IssueListView, Arti
 
 
 urlpatterns = [
-    url(r'^$', NewspaperListView.as_view(), name='newspapers'),
-    url(r'^(?P<np>[\w-]+)/$', YearListView.as_view(), name='year'),
-    url(r'^(?P<np>[\w-]+)/(?P<year>\d+)/$', IssueListView.as_view(), name='issues'),
-    url(r'^(?P<np>[\w-]+)/(?P<year>\d+)/(?P<issue>\d+)/$', ArticleListView.as_view(), name='issue'),
+    url(r'^np/$', NewspaperListView.as_view(), name='newspapers'),
+    url(r'^np/(?P<np>[\w-]+)/$', YearListView.as_view(), name='year'),
+    url(r'^np/(?P<np>[\w-]+)/(?P<year>\d+)/$', IssueListView.as_view(), name='issues'),
+    url(r'^np/(?P<np>[\w-]+)/(?P<year>\d+)/(?P<issue>\d+)/$', ArticleListView.as_view(), name='issue'),
 ]
