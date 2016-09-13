401e75c777abefebb8437448c8c675a530cb490a
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-13 13:06:33 +0300

    People

--- src/people/admin.py
+++ src/people/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
-from people.models import Person
+from people.models import Person, Face, Hair, HairColor, Haircut, HairParting, EyeColor
+
+
+class HairInline(admin.StackedInline):
+    model = Hair
+
+
+class FaceInline(admin.StackedInline):
+    model = Face
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Name', {'fields': (('first_name', 'nickname', 'last_name'), 'slug')}),
+        ('Appearance', {'fields': ('body', 'arm', 'hand', 'leg', 'foot', )}),
         ('Description', {'fields': ('description', )}),
     )
-    # inlines = [IssueInline]
+    inlines = [FaceInline, HairInline]
     prepopulated_fields = {'slug': ('first_name', 'nickname', 'last_name')}
+
+
+admin.site.register(HairColor)
+admin.site.register(Haircut)
+admin.site.register(HairParting)
+admin.site.register(EyeColor)
--- src/people/models.py
+++ src/people/models.py
@@ -2,13 +2,94 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
+class HairColor(models.Model):
+    color = models.CharField(_('color'), max_length=255)
+
+    def __unicode__(self):
+        return self.color
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class Haircut(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    size = models.IntegerField(_('size'))
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class HairParting(models.Model):
+    parting = models.CharField(_('parting'), max_length=255)
+
+    def __unicode__(self):
+        return self.parting
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class EyeColor(models.Model):
+    color = models.CharField(_('color'), max_length=255)
+
+    def __unicode__(self):
+        return self.color
+
+    def __str__(self):
+        return self.__unicode__()
+
+
+class Hair(models.Model):
+    person = models.OneToOneField('Person', default=0)
+
+    color = models.ForeignKey('HairColor', verbose_name=_('color'), blank=True, null=True)
+    haircut = models.ForeignKey('Haircut', verbose_name=_('haircut'), blank=True, null=True)
+    parting = models.ForeignKey('HairParting', verbose_name=_('parting'), blank=True, null=True)
+    curling = models.BooleanField(_('curling'), blank=True)
+    # models.ForeignKey('HairLength', verbose_name=_('length'), blank=True, null=True)
+
+
+class Face(models.Model):
+    person = models.OneToOneField('Person')
+
+    eyebrow = models.CharField(_('eyebrow'), max_length=255, blank=True)
+    eye = models.ForeignKey('EyeColor', verbose_name=_('eye'), blank=True, null=True)
+    ear = models.CharField(_('ear'), max_length=255, blank=True)
+    nose = models.CharField(_('nose'), max_length=255, blank=True)
+    chick = models.CharField(_('chick'), max_length=255, blank=True)
+    mouth = models.CharField(_('mouth'), max_length=255, blank=True)
+    lip = models.CharField(_('lip'), max_length=255, blank=True)
+    chin = models.CharField(_('chin'), max_length=255, blank=True)
+    neck = models.CharField(_('neck'), max_length=255, blank=True)
+
+
+class Cloth(models.Model):
+    person = models.OneToOneField('Person')
+
+    head = models.CharField(_('head'), max_length=255, blank=True)
+    body = models.CharField(_('body'), max_length=255, blank=True)
+    belt = models.CharField(_('belt'), max_length=255, blank=True)
+    legs = models.CharField(_('legs'), max_length=255, blank=True)
+    feet = models.CharField(_('feet'), max_length=255, blank=True)
+
+
 class Person(models.Model):
     first_name = models.CharField(_('first name'), max_length=255, blank=True)
     last_name = models.CharField(_('last name'), max_length=255, blank=True)
     nickname = models.CharField(_('nickname'), max_length=255, blank=True)
+    slug = models.SlugField(_('slug'), unique=True)
+
+    body = models.CharField(_('body'), max_length=255, blank=True)
+    arm = models.CharField(_('arm'), max_length=255, blank=True)
+    hand = models.CharField(_('hand'), max_length=255, blank=True)
+    leg = models.CharField(_('leg'), max_length=255, blank=True)
+    foot = models.CharField(_('foot'), max_length=255, blank=True)
 
     description = models.TextField(_('description'), max_length=10000, blank=True)
-    slug = models.SlugField(_('slug'), unique=True)
 
     def __unicode__(self):
         titles = [self.first_name, ]
@@ -20,6 +101,9 @@ class Person(models.Model):
 
     def __str__(self):
         return self.__unicode__()
+
+    def hair(self):
+        return self.face.hair
 
     def url(self):
         return "#"  # reverse('year', args=[self.slug])
