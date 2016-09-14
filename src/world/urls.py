4d39721a52dac764396cb7fad23f3c093fa72bf9
Dmitry Kutsenko <d2emonium@gmail.com>
2016-09-15 02:04:28 +0300

    Orphan list

--- src/world/urls.py
+++ src/world/urls.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from django.conf.urls import url
-from world.views import WorldListView, WikiListView, WorldDetailView, random_world
+from world.views import WorldListView, WikiListView, WorldDetailView, random_world, OrphanView
 
 
 urlpatterns = [
     url(r'^$', WorldListView.as_view(), name='worlds'),
     url(r'^__page.opt$', WikiListView.as_view(), name='worlds_wiki'),
     url(r'^random/$', random_world, name='random_world'),
+    url(r'^orphans/$', OrphanView.as_view(), name='world_orphans'),
     url(r'^(?P<slug>[\w-]+)/view/$', WorldDetailView.as_view(), name='world'),
 ]
--- src/world/views.py
+++ src/world/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from world.models import World
 # from django.shortcuts import render
 from django.shortcuts import redirect
@@ -7,6 +7,25 @@ from django.shortcuts import redirect
 def random_world(request):
     world = World.objects.order_by('?').first()
     return redirect(world.get_absolute_url())
+
+
+class OrphanView(TemplateView):
+    template_name = 'world/orphans_list.html'
+    path = "../media/worlds"
+
+    def get_orphans(self):
+        import os
+        try:
+            items = next(os.walk(self.path))[1]
+        except StopIteration:
+            items = []
+        items = list(filter(lambda f: not f.startswith('.') and not f.startswith('__') and not World.objects.filter(title=f).exists(), items))
+        return items
+
+    def get_context_data(self, **kwargs):
+        context = super(OrphanView, self).get_context_data(**kwargs)
+        context['orphans'] = self.get_orphans()
+        return context
 
 
 class WorldListView(ListView):
