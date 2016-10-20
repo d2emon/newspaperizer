a425898b1c95d6a97e057d19f6df1481fe83a9a9
Dmitry Kutsenko <d2emonium@gmail.com>
2016-10-20 05:25:19 +0300

    Wiki for books

--- src/book/urls.py
+++ src/book/urls.py
@@ -1,12 +1,21 @@
 # coding: utf-8
 from django.conf.urls import url
-from book.views import GenreList, GenreView, BookList, BookView
+from book.views import random_book, GenreListView, WikiListView, GenreView, BookListView, BookView
 
 
 urlpatterns = [
-    # url(r'^random/$', random_book, name='random_world'),
-    url(r'^genre/$', GenreList.as_view(), name='book_genres'),
+    url(r'^$', GenreListView.as_view()),
+
+    url(r'^genre/$', GenreListView.as_view(), name='book_genres'),
+    url(r'^orphans/$', GenreView.as_view(), name='book_genres_orphans'),
+
     url(r'^genre/(?P<slug>[\w-]+)/$', GenreView.as_view(), name='book_genre'),
-    url(r'^book/$', BookList.as_view(), name='books'),
+
+    url(r'^genre/__page.opt$', WikiListView.as_view(), name='book_genres_wiki'),
+    url(r'^genre/(?P<slug>[\w-]+)/__page.opt$', WikiListView.as_view(), name='book_genre_wiki'),
+
+    url(r'^random/(?P<genre>[\w-]+)?/?$', random_book, name='random_book'),
+    url(r'^book/$', BookListView.as_view(), name='books'),
     url(r'^book/(?P<slug>[\w-]+)/$', BookView.as_view(), name='book'),
+
 ]
--- src/book/views.py
+++ src/book/views.py
@@ -1,16 +1,27 @@
 from django.views.generic import ListView, DetailView
 from book.models import BookGenre, Book
+from django.shortcuts import redirect
 
 
-class GenreList(ListView):
+class GenreListView(ListView):
     model = BookGenre
+
+
+class WikiListView(ListView):
+    model = BookGenre
+    template_name = 'book/wiki_list.html'
 
 
 class GenreView(DetailView):
     model = BookGenre
 
 
-class BookList(ListView):
+def random_book(request):
+    book = Book.objects.order_by('?').first()
+    return redirect(book.get_absolute_url())
+
+
+class BookListView(ListView):
     model = Book
 
 
